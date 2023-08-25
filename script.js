f9385662b0310a1e19f966d7fcb8dba3677abe27
salimM4321 <139615279+salimM4321@users.noreply.github.com>
2023-08-25 11:31:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let s = document.querySelector(".S");
+let r = document.querySelector(".R");
+let g = document.querySelector(".G");
+let m = document.querySelector(".M");
+let p = document.querySelector(".P");
+
+
+
+s.addEventListener("click", function(){
+   let audio = document.createElement("audio");
+   audio.src = "mixkit-funny-cartoon-tones-2892.wav";
+  audio.autoplay = true;
+   
+});
+
+r.addEventListener("click", function(){
+   
+   let audio = document.createElement("audio");
+   audio.src = "mixkit-horror-tunnel-drum-554.wav";
+  audio.autoplay = true;
+   
+});
+
+g.addEventListener("click", function(){
+   
+   let audio = document.createElement("audio");
+   audio.src = "mixkit-joke-drums-578.wav";
+  audio.autoplay = true;
+   
+});
+
+m.addEventListener("click", function(){
+   
+   let audio = document.createElement("audio");
+   audio.src = "mixkit-tribal-dry-drum-558.wav";
+  audio.autoplay = true;
+   
+});
+
+p.addEventListener("click", function(){
+   
+   let audio = document.createElement("audio");
+   audio.src = "mixkit-joke-drums-578.wav";
+  audio.autoplay = true;
+   
+ });
+
+
